4c0c52d37501f551f285ca9adacb75d578586d65
DhavyLTS <dhavy.s@proton.me>
2024-03-12 19:29:50 -0300

    fix: clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,5 @@
-use foux::system::{component::{Mesh, Renderer}, shaders::ShaderProgram, Window};
+use foux::system::math::{Matrix4, Vector3};
 
 fn main() {
-    
-    let mut window = Window::new(800, 600, "foux engine", glfw::WindowMode::Windowed);
 
-    let fragment_shaders = std::fs::read_to_string("shaders/fragment.glsl").expect("Couldn't Read Shaders File!");
-    let vertex_shaders = std::fs::read_to_string("shaders/vertex.glsl").expect("Couldn't Read Shaders File!");
-    
-    let shaders = ShaderProgram::new(&vertex_shaders, &fragment_shaders);
-
-    let vertices: [f32; 12] = [
-        -0.5, -0.5, 0.0, // Bottom-left vertex
-         0.5, -0.5, 0.0,  // Bottom-right vertex
-         0.5,  0.5, 0.0,   // Top-right vertex
-        -0.5,  0.5, 0.0,  // Top-left vertex
-    ];
-
-    // Set up index data
-    let indices: [u32; 6] = [
-        0, 1, 2, // First triangle
-        2, 0, 3, // Second triangle
-    ];
-
-    let mesh = Mesh::new(vertices.to_vec(), indices.to_vec());
-    let renderer = Renderer::new(mesh, shaders); 
-    
-    let mut renderer_vector = Vec::new();
-    renderer_vector.push(renderer);
-
-    window.run(renderer_vector);
 }
